a40c31fe12a6c8dc1e99e7892510903f84b0d8f7
GregFaaborg <grfaaborg@gmail.com>
2019-02-15 17:12:26 -0600

    Refactor MainActivity

--- app/src/main/java/com/CSCE4901/Mint/MainActivity.java
+++ app/src/main/java/com/CSCE4901/Mint/MainActivity.java
@@ -1,8 +1,11 @@
 package com.CSCE4901.Mint;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,11 +19,14 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
 
         Button button = findViewById(R.id.login);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 
                 //switch to overview activity
                 Intent intent = new Intent(MainActivity.this, OverviewActivity.class);
