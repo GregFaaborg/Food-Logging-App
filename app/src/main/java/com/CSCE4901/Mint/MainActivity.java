341345d103db7c8771f0f199b2ac5f9f88b27a27
olivier86 <m.clench@gmail.com>
2019-02-18 10:35:15 -0600

    testing the commit function

--- app/src/main/java/com/CSCE4901/Mint/MainActivity.java
+++ app/src/main/java/com/CSCE4901/Mint/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         //TODO Remove this comment just testing git stuff more testing git
+        // this is just a test for olivier
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
