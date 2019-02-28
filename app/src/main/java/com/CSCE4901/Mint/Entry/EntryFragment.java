894686b2773949caebafc75bde469840fbac7249
GregFaaborg <grfaaborg@gmail.com>
2019-02-28 13:39:48 -0600

    Fixed create entry error

--- app/src/main/java/com/CSCE4901/Mint/Entry/EntryFragment.java
+++ app/src/main/java/com/CSCE4901/Mint/Entry/EntryFragment.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.CSCE4901.Mint.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -46,33 +51,58 @@ public class EntryFragment extends Fragment {
 
         //initialize textviews
         TITLE= view.findViewById(R.id.entry_title);
-        title = TITLE.getText().toString();
         CAT= view.findViewById(R.id.entry_category);
-        cat = CAT.getText().toString();
         DES=view.findViewById(R.id.entry_description);
-        des = DES.getText().toString();
+
+
         //initialize Buttons
         FLAG=view.findViewById(R.id.flag);
         SAVE=view.findViewById(R.id.entry_add_button);
+
         //initialize calendar view
         CAL=view.findViewById(R.id.entry_calendar);
+
         //initialize Firebase Auth instance
         firebaseAuth = FirebaseAuth.getInstance();
 
         SAVE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //get text field values
+                title = TITLE.getText().toString();
+                cat = CAT.getText().toString();
+                des = DES.getText().toString();
+
                 //get all the information in a HashMap
                 Map<String, Object> data = new HashMap<>();
                 data.put("title", title);
                 data.put("category", cat);
                 data.put("description", des);
                 data.put("flag",Flagged );
+
                 //get email of signed in user
                 FirebaseUser currentUser = firebaseAuth.getCurrentUser();
                 String UserEmail = currentUser.getEmail();
+
+
                 //save text views and flag button to database
-                db.collection(UserEmail).document("first entry").set(data);
+                db.collection(UserEmail).add(data)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("Create Entry", "DocumentSnapshot added with ID: " + documentReference.getId());
+                                Toast.makeText(getContext(), "Entry Added", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+
+                                Toast.makeText(getContext(), "Error Creating Entry", Toast.LENGTH_SHORT).show();
+                                Log.d("CREATE ENTRY", "OnFailure" ,e);
+                            }
+                        });
             }
         });
 
