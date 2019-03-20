a3157f19fd42759e866edb14bb6a7f43228634e4
olivier86 <m.clench@gmail.com>
2019-03-20 11:35:02 -0500

    Search View

--- app/src/main/java/com/CSCE4901/Mint/Search/SearchFragment.java
+++ app/src/main/java/com/CSCE4901/Mint/Search/SearchFragment.java
@@ -2,10 +2,14 @@ package com.CSCE4901.Mint.Search;
 
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,12 +18,29 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.CSCE4901.Mint.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 import static android.content.ContentValues.TAG;
 
 
 public class SearchFragment extends Fragment {
 
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance(); //point db to the root directory of the database
+    //private DatabaseReference mDatabase;
+    RecyclerView mRecyclerView;
+    LinearLayoutManager mLayoutManager;
+    SearchAdapter mAdapter;
+    private ProgressDialog progressDialog;
 
     @Nullable
     @Override
@@ -28,6 +49,16 @@ public class SearchFragment extends Fragment {
 
         final SearchView searchView = view.findViewById(R.id.search);
 
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Searching...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.search_results);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
         //TODO implement recylerview to show search results
 
@@ -58,7 +89,31 @@ public class SearchFragment extends Fragment {
             public boolean onQueryTextSubmit(String s) {
                 Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
                 searchView.clearFocus();
+                progressDialog.show();
                 Log.d(TAG, "onCreateView: in on query text submit");
+                firebaseAuth = FirebaseAuth.getInstance();
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                String UserEmail = currentUser.getEmail();
+                db.collection(UserEmail)
+                        .whereEqualTo("title", s)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                progressDialog.dismiss();
+                                if (task.isSuccessful()) {
+                                    ArrayList<SearchItem> arrItems = new ArrayList<SearchItem>();
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        arrItems.add(new SearchItem(document.getString("category"), document.getString("date"), document.getString("description"), document.getString("flag"), document.getString("title")));
+                                        Log.d(TAG, document.getId() + " => " + document.getData());
+                                    }
+                                    mAdapter = new SearchAdapter(arrItems);
+                                    mRecyclerView.setAdapter(mAdapter);
+                                } else {
+                                    Log.w(TAG, "Error getting documents.", task.getException());
+                                }
+                            }
+                        });
                 return true;
             }
 
