594810467a47d3a6704e8ddf08950211a5f58c8f
pramishathapaliya <pthapaliya143@gmail.com>
2019-04-03 00:19:12 -0500

    changing old email

--- app/src/main/java/com/CSCE4901/Mint/User/UserFragment.java
+++ app/src/main/java/com/CSCE4901/Mint/User/UserFragment.java
@@ -1,20 +1,28 @@
 package com.CSCE4901.Mint.User;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.CSCE4901.Mint.MainActivity;
 import com.CSCE4901.Mint.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -22,6 +30,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class UserFragment extends Fragment implements View.OnClickListener {
 
@@ -32,14 +41,18 @@ public class UserFragment extends Fragment implements View.OnClickListener {
     TextView last;
     TextView emailDB;
     TextView docEmail;
+    EditText etNewEmail;
 
     String email;
     String FIRST="first name";
     String LAST="last name";
     String docEMAIL="docs email";
 
+    FirebaseAuth mAuth;
+    FirebaseUser fbUser;
     FirebaseAuth firebaseAuth;
     FirebaseFirestore db = FirebaseFirestore.getInstance(); //point db to the root directory of the database
+    Context mContext;
 
 
     @Nullable
@@ -53,6 +66,14 @@ public class UserFragment extends Fragment implements View.OnClickListener {
         last=view.findViewById(R.id.last);
         emailDB=view.findViewById(R.id.email);
         docEmail=view.findViewById(R.id.doc);
+
+        //Update for email
+        etNewEmail=view.findViewById(R.id.user_email);
+        update=view.findViewById(R.id.update_button);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        fbUser = mAuth.getCurrentUser();
 
         //initialize Firebase Auth instance
         firebaseAuth = FirebaseAuth.getInstance();
@@ -93,6 +114,79 @@ public class UserFragment extends Fragment implements View.OnClickListener {
 
         return view;
     }
+    private void setUpdate() {
+
+        String oldEmail = emailDB.getText().toString();
+        String newEmail = etNewEmail.getText().toString();
+
+        if (TextUtils.isEmpty(oldEmail)) {
+
+            Toast.makeText(mContext, "Please Enter Your Old Email", Toast.LENGTH_SHORT).show();
+
+        } else if (TextUtils.isEmpty(newEmail)) {
+
+            Toast.makeText(mContext, "Please Enter Your New Email", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            String email = fbUser.getEmail();
+
+            if (!oldEmail.equals(email)) {
+
+                Toast.makeText(mContext, "Wrong Current Email, Please Check Again", Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                fbUser.updateEmail(newEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful()) {
+
+                            fbUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    Toast.makeText(mContext, "Verification Email Sent To Your Email.. Please Verify and Login", Toast.LENGTH_LONG).show();
+
+                                    // Logout From Firebase
+                                    FirebaseGeneral firebaseGeneral = new FirebaseGeneral();
+                                    firebaseGeneral.logoutUser(mContext);
+
+                                }
+                            });
+
+                        } else {
+
+                            try {
+                                throw Objects.requireNonNull(task.getException());
+                            }
+
+                            // Invalid Email
+                            catch (FirebaseAuthInvalidCredentialsException malformedEmail) {
+                                Toast.makeText(mContext, "Invalid Email...", Toast.LENGTH_LONG).show();
+
+                            }
+                            // Email Already Exists
+                            catch (FirebaseAuthUserCollisionException existEmail) {
+                                Toast.makeText(mContext, "Email Used By Someone Else, Please Give Another Email...", Toast.LENGTH_LONG).show();
+
+                            }
+                            // Any Other Exception
+                            catch (Exception e) {
+                                Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+
+                    }
+                });
+
+            }
+
+        }
+
+    }
 
     @Override
     public void onClick(View v) {
@@ -132,6 +226,7 @@ public class UserFragment extends Fragment implements View.OnClickListener {
                 db.collection("users").document(email).set(data);
                 //TOOOOO DOOOOO
                 //do the firebase auth email change
+
 
         }
 
