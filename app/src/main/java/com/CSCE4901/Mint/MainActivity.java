c4ea19b2908cbd1a8de6fb36f506fc974cfc57b9
GregFaaborg <grfaaborg@gmail.com>
2019-04-06 23:50:56 -0500

    Refactored storage access permissions for reports and got rid of some unused imports in other activities

--- app/src/main/java/com/CSCE4901/Mint/MainActivity.java
+++ app/src/main/java/com/CSCE4901/Mint/MainActivity.java
@@ -1,26 +1,22 @@
 package com.CSCE4901.Mint;
+
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
-import android.support.v4.content.pm.PackageInfoCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -29,7 +25,6 @@ import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 import java.util.TimerTask;
-import java.util.Vector;
 
 
 public class MainActivity extends AppCompatActivity {
